52e35cc577b6d0d50e43ec9a4d1510a3865c2064
ydszq <123632704@qq.com>
2020-05-30 20:23:13 +0800

    用户界面设计ing

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -6,23 +6,20 @@
 			</view>
 		</view>
 		<view class="user_top">
-			<view class="user-box">
+			<view class="user_top-box">
 				<view class="user_headimg" :style="'background-image:url('+user.headimg+')'" :mode="user.name"></view>
 				<view class="user_name">
 					{{user.name}}
 				</view>
 			</view>
 		</view>
-		<br>
-		<br>
-		<br>
-		<view class="user-box">
-			<view class="">
-				我的收藏
-			</view>
-			<view class="">
-				我的足迹
-			</view>
+		<view class="user_content-box">
+			
+		</view>
+		<view class="user_content-box">
+			<uni-list>
+				<uni-list-item title="标题文字" note="描述信息"></uni-list-item>
+			</uni-list>
 		</view>
 	</view>
 </template>
@@ -43,16 +40,16 @@
 <style>
 	.user_top {
 		width: 100%;
-		height: 10vw;
-		padding-top: 20vw;
 		background-color: #F0AD4E;
-		color: #555;
 		font-size: 5vw;
+		padding: 5vw 0;
+		border-radius: 0 0 10vw 10vw;
 	}
 
 	.user_name {
 		color: #333;
 		font-size: 8vw;
+		font-weight: 600;
 		margin-left: 25vw;
 		margin-top: 5vw;
 		position: absolute;
@@ -75,11 +72,15 @@
 		position: absolute;
 	}
 
-	.user-box {
+	.user_top-box {
 		width: 90%;
 		height: 20vw;
 		margin: auto;
-		border-radius: 5vw;
+	}
+	.user_content-box {
+		width: 90%;
+		margin: 1vw auto;
+		border-radius: 2vw;
 		background-color: #fff;
 		box-shadow: 0 1px 5px #bbb;
 	}
